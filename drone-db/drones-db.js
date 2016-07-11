8ae20447e5eed41dc810d350d5aeba030fa846d7
UNFPAmaldives <curran@unfpa.org>
2016-07-11 06:05:28 +0500

    Nth pages commit

--- drone-db/drones-db.js
+++ drone-db/drones-db.js
@@ -687,9 +687,10 @@ $(function(){
     }
     function formatDate(date, new_delimiter){
         var new_date = new Date(date);
-        return new_date.getFullYear() ? new_date.getFullYear() + '-' + (new_date.getMonth() + 1) + '-' + new_date.getDate() : 'unknown';
+        return new_date.getFullYear() ? new_date.getFullYear() + '-' +  '-' + new_date.getDate() : 'unknown';
     }
     function formatDatePostStyle(date){
+        var months_styled = ['Jan.', 'Feb.', 'March', 'April', 'May', 'June', 'July', 'Aug.', 'Sept.', 'Oct.', 'Nov.', 'Dec.'];
         var is_classified = date.indexOf('-') > -1;
         var new_date = is_classified ? new Date(date.split(' - ')[0]) : new Date(date);
         return is_classified ? date
